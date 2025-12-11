2cc538724b2d20b9ec7c1fab30fc78344bd27f56
Khushiyant <khushiyant2002@gmail.com>
2025-12-11 13:31:24 +0100

    fix: update Python version in workflow and dynamically read project version from pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,15 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'SheetWise'
 copyright = '2025, Khushiyant Chauhan'
 author = 'Khushiyant Chauhan'
-release = '2.3.0'
+
+# Read version from pyproject.toml
+import tomli
+try:
+    with open(os.path.abspath('../../pyproject.toml'), 'rb') as f:
+        pyproject = tomli.load(f)
+        release = pyproject['project']['version']
+except Exception:
+    release = '2.5.0'  # Fallback version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
